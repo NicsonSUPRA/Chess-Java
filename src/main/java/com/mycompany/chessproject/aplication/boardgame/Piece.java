7749b01c8433d0b2b6765b32e4644db9ad3b60d4
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-03-17 14:59:41 -0300

    Start to implement the board

---
+++ src/main/java/com/mycompany/chessproject/aplication/boardgame/Piece.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chessproject.aplication.boardgame;
+
+/**
+ *
+ * @author nicsondev
+ */
+public class Piece {
+    protected Position position;
+    private Board board;
+    
+    public Piece(Board board){
+        this.board = board;
+        position = null;
+    }
+    
+    public Board getBoard(){
+        return board;
+    }
+}
